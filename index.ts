b33bc67f1cb0ce61a23c9db974170e48eec806c7
josephDev123 <josephuzuegbu55@gmail.com>
2024-10-24 17:44:40 +0100

    fix dev port

--- index.ts
+++ index.ts
@@ -45,7 +45,7 @@ const startApp = async () => {
     app.use(ErrorHandlerMiddleware);
 
     app.listen(process.env.PORT, () => {
-      console.log(`listening on port ${process.env.PORT || 80}`);
+      console.log(`listening on port ${process.env.PORT || 5000}`);
     });
   } catch (error) {
     console.log(error);
